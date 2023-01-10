42c1a1dce0e182eb40829cf626eecfb1d0bb9b84
akshitha123hub <121024936+akshitha123hub@users.noreply.github.com>
2023-01-10 17:03:51 +0530

    issue resolved

--- newgit.py
+++ newgit.py
@@ -2,13 +2,13 @@ def user_calculator():
     num1=(input("enter a number: "))
     num2=(input("enter second number: "))
     try:
-        a=int(num1)
-        b=int(num2)
+        a=float(num1)
+        b=float(num2)
         sum1=a+b
         return(sum1)
     except ValueError:
         return("you entered a string, enter a number")
 print(user_calculator())
    
- 
+#change int to float so now it will work for both integers and floating point numbers 
    
